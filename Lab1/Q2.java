e70ba3bd7000c3b3573d6a1cd7f8e2abdf5a5978
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-02-05 16:48:57 +0200

    Lab 1 done

---
+++ Lab1/Q2.java
@@ -0,0 +1,7 @@
+public class Q2 {
+    public static void main(String[] args) {
+        if (args[0].equals("CoreJava")) {
+            System.out.println("True Value " + " " + args[0]);
+        }
+    }
+}
